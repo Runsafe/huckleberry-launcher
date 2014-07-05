b835e0d139a5e9c16bc6010a03801d04b1a015db
Morten Nilsen <morten@runsafe.no>
2014-07-05 15:27:28 +0200

    Colorize output

--- unix_launch.sh
+++ unix_launch.sh
@@ -5,33 +5,34 @@ run()
 {
 	login
 	fetchinventory
-	parallelize updatelib "Updating libraries" "${libs[@]}"
-	parallelize mkassetdir "Creating asset folders" "${asset_dirs[@]}"
-	parallelize updateasset "Updating asset files" "${assets[@]}"
+	parallelize updatelib "${GREEN}Updating libraries${RESET}" "${libs[@]}"
+	parallelize mkassetdir "${GREEN}Creating asset folders${RESET}" "${asset_dirs[@]}"
+	parallelize updateasset "${GREEN}Updating asset files${RESET}" "${assets[@]}"
 	startclient 1.6.4
 }
 
 login()
 {
 	if [ -z "$un" ]; then
-		read -p "Enter your Huckleberry username: " un
+		read -p "`echo -e ${BRIGHT_CYAN}Enter your ${BRIGHT_GREEN}Huckleberry ${BRIGHT_CYAN}username:${YELLOW}`" un
 	fi
 	if [ -z "$pn" ]; then
-		read -sp "Enter your Huckleberry password: " pn
+		read -sp "`echo -e ${BRIGHT_CYAN}Enter your ${BRIGHT_GREEN}Huckleberry ${BRIGHT_CYAN}password:`" pn
 		echo
 	fi
+	echo -ne $RESET
 
 	UUID=$(curl -s "https://huckleberry.runsafe.no/auth.php?username=${un}&password=${pn}")
 
 	if [ $UUID = 'invalid' ]; then
-		echo "Invalid username and/or password"
-		read -p "Press [Enter] to exit."
+		echo -e "${REV_RED}Invalid username and/or password${RESET}"
+		read -p "`echo -e Press ${CYAN}[Enter]${RESET} to exit.`"
 		exit
 	fi
 
 	if [ $UUID = 'noinvite' ]; then
-		echo "You have not been invited to Huckleberry yet!"
-		read -p "Press [Enter] to exit."
+		echo -e "${REV_RED}You have not been invited to Huckleberry yet!${RESET}"
+		read -p "`echo -e Press ${CYAN}[Enter]${RESET} to exit.`"
 		exit
 	fi
 
@@ -42,10 +43,13 @@ login()
 
 fetchinventory()
 {
-	IFS="`echo -e '\x1F'`" read -a asset_data <<< "`curl -s "https://huckleberry.runsafe.no/assets.dat"`"
-	IFS="`echo -e '\x1E'`" read -a asset_dirs <<< "${asset_data[0]}"
-	IFS="`echo -e '\x1E'`" read -a assets <<< "${asset_data[1]}"
-	IFS="," read -a libs <<< "`curl -s "https://huckleberry.runsafe.no/lib_list.php"`"
+	local IFS=`echo -e '\x1F'`
+	read -a asset_data <<< "`curl -s "https://huckleberry.runsafe.no/assets.dat"`"
+	local IFS=`echo -e '\x1E'`
+	read -a asset_dirs <<< "${asset_data[0]}"
+	read -a assets <<< "${asset_data[1]}"
+	local IFS=","
+	read -a libs <<< "`curl -s "https://huckleberry.runsafe.no/lib_list.php"`"
 }
 
 parallelize()
@@ -64,7 +68,7 @@ parallelize()
 		fi
 	done
 	wait
-	echo done!
+	echo -e "${BRIGHT_GREEN}done!${RESET}"
 	
 }
 
@@ -87,10 +91,10 @@ updatelib()
 mkassetdir()
 {
 	if [ ! -d "${path}assets/$1" ]; then
-		output N "Creating folder $1"
+		output "${BRIGHT_GREEN}N${RESET}" "Creating folder: ${BRIGHT_GREEN}$1${RESET}"
 		mkdir -p "${path}assets/$1"
 	else
-		output . "Skipping folder $1" 2
+		output . "Skipping folder: ${YELLOW}$1${RESET}" 2
 	fi
 }
 
@@ -112,28 +116,28 @@ updatefile()
 	target=$3
 	if [ -f $target ]; then
 		if [ ! -f "${target}.md5" -o "${target}.md5" -ot "${target}" ]; then
-			output '#' "Rehashing local file ${target}"
+			output "${BRIGHT_RED}#${RESET}" "Rehashing local file ${BRIGHT_RED}${target}${RESET}"
 			__rvm_md5_for "$target" > "${target}.md5"
 		fi
 		local_hash=$(cat "${target}.md5")
 	fi
 	if [ -n "$remote_hash" -a $remote_hash != "$local_hash" ]; then
 		if [ -z "$local_hash" ]; then
-			output N "Downloading new file from $sourceuri"
+			output "${BRIGHT_GREEN}N${RESET}" "Downloading new file from ${BRIGHT_GREEN}$sourceuri${RESET}"
 		else
-			output U "Downloading modified file from $sourceuri"
+			output "${YELLOW}U${RESET}" "Downloading modified file from ${YELLOW}$sourceuri${RESET}"
 		fi
 		curl -s -o "$target" $sourceuri
 		__rvm_md5_for "$target" > "${target}.md5"
 		local_hash=$(cat "${target}.md5")
 		if [ $remote_hash != "$local_hash" ]; then
 			echo
-			echo "Wrong hash downloading resource $sourceuri, aborting!"
+			echo "${REV_RED}Wrong hash downloading resource $sourceuri, aborting!${RESET}"
 			rm -f "$target"
 			exit 1
 		fi
 	else
-		output . "Skipping up to date file $sourceuri" 2
+		output . "Skipping up to date file ${BRIGHT_BLACK}$sourceuri${RESET}" 2
 	fi
 }
 
@@ -160,12 +164,12 @@ join()
 output()
 {
 	if [ $verbose -eq 0 ]; then
-		echo -n $1
+		echo -ne $1
 	elif [ -z "$3" ] || [ "$verbose" -ge "$3" ]; then
 		if [ -z "$2" ]; then
-			echo $1
+			echo -e $1
 		else
-			echo $2
+			echo -e $2
 		fi
 	fi
 }
@@ -203,5 +207,47 @@ lib_dir="${path}libs/"
 if [ ! -d "$lib_dir" ]; then
   mkdir -p "$lib_dir"
 fi
+
+DULL=0
+BRIGHT=1
+FG_BLACK=30
+FG_RED=31
+FG_GREEN=32
+FG_YELLOW=33
+FG_BLUE=34
+FG_VIOLET=35
+FG_CYAN=36
+FG_WHITE=37
+FG_NULL=00
+BG_BLACK=40
+BG_RED=41
+BG_GREEN=42
+BG_YELLOW=43
+BG_BLUE=44
+BG_VIOLET=45
+BG_CYAN=46
+BG_WHITE=47
+BG_NULL=00
+ESC="\001\e"
+NORMAL="$ESC[m\002"
+RESET="$ESC[${DULL};${FG_WHITE};${BG_NULL}m\002"
+BLACK="$ESC[${DULL};${FG_BLACK}m\002"
+RED="$ESC[${DULL};${FG_RED}m\002"
+GREEN="$ESC[${DULL};${FG_GREEN}m\002"
+YELLOW="$ESC[${DULL};${FG_YELLOW}m\002"
+BLUE="$ESC[${DULL};${FG_BLUE}m\002"
+VIOLET="$ESC[${DULL};${FG_VIOLET}m\002"
+CYAN="$ESC[${DULL};${FG_CYAN}m\002"
+WHITE="$ESC[${DULL};${FG_WHITE}m\002"
+BRIGHT_BLACK="$ESC[${BRIGHT};${FG_BLACK}m\002"
+BRIGHT_RED="$ESC[${BRIGHT};${FG_RED}m\002"
+BRIGHT_GREEN="$ESC[${BRIGHT};${FG_GREEN}m\002"
+BRIGHT_YELLOW="$ESC[${BRIGHT};${FG_YELLOW}m\002"
+BRIGHT_BLUE="$ESC[${BRIGHT};${FG_BLUE}m\002"
+BRIGHT_VIOLET="$ESC[${BRIGHT};${FG_VIOLET}m\002"
+BRIGHT_CYAN="$ESC[${BRIGHT};${FG_CYAN}m\002"
+BRIGHT_WHITE="$ESC[${BRIGHT};${FG_WHITE}m\002"
+REV_CYAN="$ESC[${DULL};${BG_WHITE};${BG_CYAN}m\002"
+REV_RED="$ESC[${DULL};${FG_YELLOW}; ${BG_RED}m\002"
 
 run
